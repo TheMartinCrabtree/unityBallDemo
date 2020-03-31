3d0df6f9994825a60f1b25520eef3c36a0dace41
MC-Games\MC <martin.crabtree@gmail.com>
2020-03-31 16:21:57 -0400

    testing isometric camera linked to player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
 
+    private Vector3 cameraOffset;
 
+    public void Start()
+    {
+        // on game start, set a static follow distance for the camera to the player
+        cameraOffset = transform.position - player.transform.position;
+    }
+
+    public void LateUpdate()
+    {
+
+        transform.position = player.transform.position + cameraOffset;
+    }
 
 
 }
